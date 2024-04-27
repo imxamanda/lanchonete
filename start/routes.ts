d906e8ef33fafb869b7b81299b5047fad4fdf276
imxamanda <amandavieiraperes.12@gmail.com>
2024-04-26 21:52:39 -0300

    adonis aula1

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,12 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import ProdutosController from '../app/controllers/produtos_controller.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.resource('/produtos', ProdutosController).apiOnly()   
