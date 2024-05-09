b17f7dc2e19c2deb6a0b2b06d53ddd707f9747fc
imxamanda <amandavieiraperes.12@gmail.com>
2024-05-08 21:49:16 -0300

    Done

--- app/controllers/produtos_controller.ts
+++ app/controllers/produtos_controller.ts
@@ -5,7 +5,7 @@ import Produto from "../models/produto.js";
 export default class ProdutosController {
 
     async index(){
-        return await Produto.all()
+        return await Produto.query().paginate()
     }
 
 }
--- start/routes.ts
+++ start/routes.ts
@@ -8,7 +8,10 @@
 */
 
 import router from '@adonisjs/core/services/router'
+
+import IngredientesController from '../app/controllers/ingredientes_controller.js'
 import ProdutosController from '../app/controllers/produtos_controller.js'
+import TiposController from '../app/controllers/tipos_controller.js'
 
 router.get('/', async () => {
   return {
@@ -17,3 +20,5 @@ router.get('/', async () => {
 })
 
 router.resource('/produtos', ProdutosController).apiOnly()   
+router.resource('/tipos', TiposController).apiOnly()   
+router.resource('/ingredientes', IngredientesController).apiOnly()   
